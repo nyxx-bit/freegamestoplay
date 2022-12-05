341162197945594f1bb2fcbca0a9ae6816cc8646
nyxx-bit <alkhawarismi989@gmail.com>
2022-12-06 01:36:03 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/pages/Home";
+import PC from "./components/pages/PC";
+import Browser from "./components/pages/Browser";
+import About from "./components/pages/About";
+import Login from "./components/pages/Login";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const token = localStorage.getItem("authenticated");
+
+  console.log(token);
+
+  if (!token) {
+    return <Login />;
+  } else {
+    return (
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/pc" element={<PC />} />
+          <Route path="/browser" element={<Browser />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    );
+  }
+};
 
 export default App;
